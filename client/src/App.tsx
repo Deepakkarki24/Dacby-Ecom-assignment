a716443144ec5f7ed756cbd0e65d095e25bb1605
Deepak Karki <ideepakkarki@gmail.com>
2026-07-18 16:08:48 +0530

    update: setup frontend with (react + typeScript + tailwind)

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-2xl text-green-400 font-semibold'>
+      E-comm project
+    </div>
+  )
+}
+
+export default App
